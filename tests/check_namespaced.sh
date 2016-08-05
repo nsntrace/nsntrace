393dc473e9188c25b15981316cea86eb4d5d9424
Chris Lamb <chris@chris-lamb.co.uk>
2016-08-05 19:10:39 +0100

    Always stop udp_send, even on failure (#13)

--- tests/check_namespaced.sh
+++ tests/check_namespaced.sh
@@ -7,13 +7,13 @@ filter="udp port 1337"
 # start sending packages non-namespaced
  ./udp_send 1337 -1 &
 
+# stop the non-namespaced udp_send on exit
+trap "kill $(pidof udp_send)" EXIT
+
 # make sure we only capture the packets from the namespaced version
 sudo ../src/nsntrace -d lo -f "$filter" $cmd | grep "$packets packets" || {
     echo "Did not capture $packets packets!"
     exit 1
 }
-
-# stop the non-namespaced udp_send
-kill $(pidof udp_send)
 
 exit 0
